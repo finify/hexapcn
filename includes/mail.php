6caa2fce489375e6343db3263c9121a1d671c138
finify <ifeanyiizuegbu@yahoo.com>
2022-09-21 20:19:40 +0100

    config

--- includes/mail.php
+++ includes/mail.php
@@ -17,15 +17,15 @@ function mailto($to,$subject,$message){
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'ssl://smtp.titan.email';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'admin@pinecapitalnetwork.com';                     //SMTP username
-        $mail->Password   = '@zwiD6?uTef5Brs';                               //SMTP password
+        $mail->Username   = 'support@pinecapitalnetwork.com';                     //SMTP username
+        $mail->Password   = 'MhQ2icI4kj';                               //SMTP password
         $mail->SMTPSecure = 'SSL';            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
         //Recipients
-        $mail->setFrom('admin@pinecapitalnetwork.com', 'pinecapitalnetwork');
+        $mail->setFrom('support@pinecapitalnetwork.com', 'pinecapitalnetwork');
         $mail->addAddress($to);               //Name is optional
-        $mail->addReplyTo('admin@pinecapitalnetwork.com', 'pinecapitalnetwork');
+        $mail->addReplyTo('support@pinecapitalnetwork.com', 'pinecapitalnetwork');
       
     
       
